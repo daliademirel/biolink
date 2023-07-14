a86d56de1da3a8301b1d2be0aed3a906e20b2ee4
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-14 20:13:35 +0700

    Update ExternalLink.vue

--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -1,13 +1,13 @@
 <template>
   <li>
     <nuxt-link :to="url" target="_blank" v-if="label && url">
-      <dt class="flex items-center space-x-2 p-1 -m-1 rounded-xl hover:bg-slate-100 bg-slate-50">
+      <dt class="flex items-center space-x-2 p-1 -m-1 rounded-xl hover:bg-slate-100 bg-slate-50 text-base">
         <div class="flex-shrink-0 flex h-10 w-10 items-center justify-center rounded-lg text-slate-500">
           <icon v-if="icon" :name="icon" class="h-5 w-5" />
           <icon v-else name="ph:link-simple" class="h-5 w-5" />
         </div>
         <div class="w-full flex-grow min-w-0">
-          <p class="font-medium text-base leading-6 text-gray-900">
+          <p class="font-medium leading-6 text-gray-900">
             {{ label }}
           </p>
         </div>
