b580930804b6377e14bbc7c8f471aab499d809fc
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-14 17:54:17 +0700

    Update transformer.js

--- utils/transformer.js
+++ utils/transformer.js
@@ -4,5 +4,6 @@ export const encodeData = (obj) => {
   return encode(encodeURIComponent(JSON.stringify(obj)));
 };
 
-export const decodeData = (base64) => JSON.parse(decodeURIComponent(decode(base64)));
+export const decodeData = (base64) => {
+  return JSON.parse(decodeURIComponent(decode(base64)));
 };
