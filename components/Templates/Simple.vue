ff9123c2e943e8b6d3d5dd7b73b27c3241030c08
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-14 23:09:38 +0700

    Update Simple.vue

--- components/Templates/Simple.vue
+++ components/Templates/Simple.vue
@@ -104,4 +104,11 @@ const allSocialLinksAreEmpty = computed(() => {
   );
 });
 </script>
-<style scoped></style>
+<style scoped>
+main {
+  background-image: url('https://i.ibb.co/DR2Cqdc/IMG-8240.jpg');
+  background-blend-mode: lighten;
+  background-color: rgba(255, 255, 255, 0.6);
+}
+</style>
+
