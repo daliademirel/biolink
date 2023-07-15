27f6ebfc7fa3e02af9f412a1f5716b35bacb564f
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-15 23:05:28 +0700

    Update ExternalLink.vue

--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <nuxt-link :to="url" target="_blank" v-if="label && url">
+    <nuxt-link :to="url" target="_blank">
       <dt
         class="flex items-center space-x-2 p-1 -m-1 rounded-xl hover:bg-slate-100 bg-slate-50"
       >
@@ -20,6 +20,13 @@
         </div>
       </dt>
     </nuxt-link>
+
+    <!-- Popup-Code hier eingefügt und als Button angepasst -->
+    <div class="coupon-popup" :class="{ 'show': showPopup }" @click.self="closePopup">
+      <button class="card" :class="{ 'slide-in': showPopup }" @click="closePopup">
+        Close
+      </button>
+    </div>
   </li>
 </template>
 
@@ -41,4 +48,56 @@ const props = defineProps({
     type: String,
   },
 });
+
+const showPopup = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    showPopup.value = true;
+  }, 5000);
+});
+
+function closePopup() {
+  showPopup.value = false;
+}
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+.coupon-popup {
+  /* Ändern Sie die Position auf "absolute" und die Höhe und Breite auf "auto" */
+  position: absolute;
+  width: auto;
+  height: auto;
+  /* Entfernen Sie die Hintergrundfarbe und die Ausrichtungseigenschaften */
+  z-index: 9999;
+}
+
+.card {
+  /* Ändern Sie die Breite und Höhe auf kleinere Werte, um einen Button darzustellen */
+  width: 100px;
+  height: 50px;
+  /* Ändern Sie die Hintergrundfarbe und den Schatten, um einen Button darzustellen */
+  background-color: #00aeef;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  /* Zentrieren Sie den Text im Button */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* Fügen Sie eine Randradius hinzu, um den Button abgerundet zu machen */
+  border-radius: 25px;
+  /* Fügen Sie eine Schriftfarbe hinzu */
+  color: white;
+  /* Fügen Sie eine Schriftgröße hinzu */
+  font-size: 14px;
+  /* Fügen Sie einen Cursor hinzu */
+  cursor: pointer;
+}
+</style>
