61740be69c52330a2bd790e01ee14e2dbd8e6c0c
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-15 20:24:29 +0700

    Update 1.vue

--- pages/1.vue
+++ pages/1.vue
@@ -1,34 +1,31 @@
 <template>
   <div>
-    <Popup v-if="showPopup" />
     <templates-simple v-if="decodedData" :acc="decodedData" />
-    <div v-else class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div
+      v-else
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+    >
       <base-loading class="h-5 w-5" />
     </div>
   </div>
 </template>
-
 <script setup>
 import { onMounted, ref } from 'vue';
 import { decodeData } from "../utils/transformer";
 import axios from 'axios';
-import Popup from './popup.vue';
 const route = useRoute();
 const acc = route.query.data;
 const decodedData = ref({});
-const showPopup = ref(false);
 
 onMounted(async () => {
+  // Erhalten Sie die IP-Adresse und den Standort des Benutzers
   const response = await axios.get('https://api.ipapi.com/check?access_key=c886500afafa455c348bfdbae47b9522');
   const location = response.data.city || response.data.region_name || response.data.location.capital;
   const flagEmoji = response.data.location.country_flag_emoji;
   decodedData.value = decodeData(acc);
+  // Ersetzen Sie "*CITY*" durch den Standort und das Emoji
   if (decodedData.value.d.includes('*CITY*')) {
     decodedData.value.d = decodedData.value.d.replace('*CITY*', `${location} ${flagEmoji}`);
   }
-
-  setTimeout(() => {
-    showPopup.value = true;
-  }, 5000);
 });
 </script>
