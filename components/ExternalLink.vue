40c5532aa5cf9b5ec01974bf3f1eeaa944043200
daliademirel <132152964+daliademirel@users.noreply.github.com>
2023-07-15 22:46:30 +0700

    Update ExternalLink.vue

--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -14,9 +14,6 @@
           <p class="font-medium text-xl leading-6 text-gray-900">
             {{ label }}
           </p>
-          <div v-if="videoUrl" class="mt-2">
-            <a :href="videoUrl" target="_blank">Watch Video</a>
-          </div>
         </div>
       </dt>
     </nuxt-link>
